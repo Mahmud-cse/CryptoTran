bebe0ae675aca60b4ff1981c6a38098a5bde83b2
Mahmud-cse <mahmudhasan555666@gmail.com>
2022-07-22 21:07:32 +0600

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 // import { useState } from 'react'
 // import './App.css'
-
+import { Navbar,Welcome,Footer,Services,Transactions } from './components';
 const App=()=> {
   // const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
     </div>
   )
 }
